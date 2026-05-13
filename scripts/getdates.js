9b560bd9b20accbbc907100a0a2748d196f6f0c4
Andres Pluska <pluskaandres@gmail.com>
2026-05-13 18:26:39 -0500

    First week

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById("lastmodified").textContent = `Last Modified: ${document.lastModified}`;
